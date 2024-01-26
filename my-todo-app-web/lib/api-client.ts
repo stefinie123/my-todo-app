010d7b684a3a878a152156a3674224981575622c
Kavith Lokuhewage <kaviththiranga@gmail.com>
2024-01-26 14:41:37 +0530

    Add front-end application

---
+++ my-todo-app-web/lib/api-client.ts
@@ -0,0 +1,53 @@
+export interface ToDo {
+  id: string;
+  task: string;
+}
+
+class ApiClient {
+  baseUrl: string;
+
+  constructor() {
+    this.baseUrl = process.env.TODO_BACKEND_URL || '';
+  }
+
+  async getTodos() {
+    const response = await fetch(`${this.baseUrl}/todos`);
+    return response.json();
+  }
+
+  async createTodo(todo: ToDo) {
+    const response = await fetch(`${this.baseUrl}/todos`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(todo),
+    });
+    return response.json();
+  }
+
+  async updateTodo(id: string, todo: ToDo) {
+    const response = await fetch(`${this.baseUrl}/todos/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(todo),
+    });
+    return response.json();
+  }
+
+  async deleteTodo(id: string) {
+    const response = await fetch(`${this.baseUrl}/todos/${id}`, {
+      method: 'DELETE',
+    });
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+    return response.json();
+  }
+}
+
+const apiClientInstance = new ApiClient();
+
+export default apiClientInstance;
