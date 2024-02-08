24b0f0acfea643fe8ea14aebe50b24e2708ba90c
Stefinie Fernando <minolispencer@gmail.com>
2024-02-09 02:40:29 +0530

    Update styles and add functionality

--- my-todo-app-web/app/create/page.tsx
+++ my-todo-app-web/app/create/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import React from "react";
-// import { createTodo } from "./actions";
-import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export default function CreatePage() {
@@ -26,15 +24,13 @@ export default function CreatePage() {
         } catch (error) {
             throw new Error(`Error: ${error}`);
         }
-    
-        // revalidatePath('/');
         redirect('/');
     }
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-                <h1 className="text-3xl font-bold">Create ToDo</h1>
+            <div className="mb-32 flex flex-col gap-y-4 items-center">
+                <h1 className="text-3xl font-bold text-gray-700">Create ToDo</h1>
                 {error && <p className="text-red-500">{error}</p>}
                 <form action={createTodo} className="mt-8">
                     <div className="mb-4">
@@ -49,8 +45,8 @@ export default function CreatePage() {
                         </label>
                         <input name="task" required type="text" value={taskLabel} onChange={e => setTaskLabel(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
                     </div>
-                    <div className="flex items-center justify-between">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                    <div className="flex items-center justify-center">
+                        <button className="bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                             Save
                         </button>
                     </div>
--- my-todo-app-web/app/page.tsx
+++ my-todo-app-web/app/page.tsx
@@ -2,43 +2,124 @@
 
 import { ToDo } from "@/types/todo";
 import Link from "next/link";
+import Image from "next/image";
 import "./globals.css";
-// import apiClientInstance, { ToDo } from "../lib/api-client";
 import { useEffect, useState } from "react";
+import checkList from "@/public/checklist.png";
+import addIcon from "@/public/addIcon.png";
+import deleteIcon from "@/public/delete.png";
 
-const TodoItem = ( props: { todo: ToDo }) => (
-  <div className="border border-gray-300 p-4 my-4 rounded bg-gray-100 shadow-md">
-    <h2 className="border-b border-gray-400 pb-2 mb-4 text-lg font-bold text-gray-700">ID: {props.todo.id}</h2>
-    <p className="text-base text-gray-700">Task: {props.todo.task}</p>
-  </div>
-);
+interface TodoItemProps {
+  todo: ToDo;
+  onDelete: (id: number) => void;
+  handleToggleDone: (id: number) => void;
+}
+
+const TodoItem = (props: TodoItemProps) => {
+  const { todo, onDelete, handleToggleDone } = props;
+  return (
+    <div className="flex justify-between bg-white rounded-full p-1 w-2/5">
+      <li
+        key={todo.id}
+        className="relative rounded-md py-3 px-5 flex items-center gap-x-3"
+      >
+        <input
+          type="checkbox"
+          checked={todo.done}
+          className="size-6"
+          onChange={() => handleToggleDone(todo.id!)}
+        />
+        <h2
+          className={
+            todo.done
+              ? "text-md font-medium leading-5 text-gray-700 line-through"
+              : "text-md font-medium leading-5 text-gray-700"
+          }
+        >
+          {todo.task}
+        </h2>
+      </li>
+      <button
+        className="float-right bg-red-500 text-white rounded-full self-center text-xs p-2 mr-2"
+        onClick={() => onDelete(todo.id!)}
+      >
+        <Image src={deleteIcon} alt="delete icon" />
+      </button>
+    </div>
+  );
+};
 
 export default function Home() {
-
-//   const todos = await fetch('/api/todos').then(res => res.json());
 
   const [todos, setTodos] = useState<ToDo[]>([]);
 
   useEffect(() => {
-    fetch('/api/todos')
-      .then(res => res.json())
-      .then(data => setTodos(data));
+    fetch("/api/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
   }, []);
-  
+
+  const handleDeleteTodo = (id: number) => {
+    fetch("/api/todos", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id }),
+    })
+      .then((res) => res.json())
+      .then(() => setTodos(todos.filter((todo) => todo.id !== id)));
+  };
+
+  const handleToggleDone = (id: number) => {
+    fetch("/api/todos", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id }),
+    })
+      .then((res) => res.json())
+      .then(() =>
+        setTodos(
+          todos.map((todo) =>
+            todo.id === id ? { ...todo, done: !todo.done } : todo
+          )
+        )
+      );
+  };
+
   return (
-    <main className="flex min-h-screen items-center justify-center p-24">
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left mx-auto">
-        {todos.map((todo: ToDo) => (
-          <TodoItem key={todo.id} todo={todo} />
-        ))}
+    <main className="flex flex-col min-h-screen items-center justify-center p-24 w-screen h-screen">
+      <div className="flex gap-x-4  my-24 items-center">
+        <Image src={checkList} alt="checklist icon" />
+        <h1 className="text-5xl font-bold text-gray-700">ToDo List </h1>
+      </div>
+
+      <div className="mb-32 mx-auto flex-col flex gap-y-2 w-full justify-center">
+        <ul className="flex-col flex gap-y-2 w-full justify-center items-center">
+          {todos.map((todo: ToDo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onDelete={handleDeleteTodo}
+              handleToggleDone={handleToggleDone}
+            />
+          ))}
+        </ul>
         {todos.length == 0 && <div className="text-center">No ToDos Found</div>}
         <div className="mt-8 text-center">
-          <Link href="/create" className="inline-block bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 mx-auto">
-            Create New
+          <Link
+            href="/create"
+            className="inline-block bg-blue-900 border-blue-900 text-white py-2 px-4 rounded-full hover:bg-blue-700 mx-auto"
+          >
+            <div className="flex items-center gap-x-2">
+                <Image src={addIcon} alt="checklist icon" />
+                Create New
+            </div>
           </Link>
         </div>
       </div>
     </main>
   );
 }
-
